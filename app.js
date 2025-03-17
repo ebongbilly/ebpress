3ddcaf304433d4ed2ee6374083c5b968b7391133
Ebong Billy <ebongbilly24@gmail.com>
2025-03-17 18:13:50 +0100

    began working on the views

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
+
+// Serve static files from the "public" directory
+app.use(express.static('./public'))
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey Class', message: 'How fast can this system be?' });
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('about', { title: 'Dashboard' });
+});
+
+app.get('/posts', (req, res) => {
+    res.render('about', { title: 'Posts' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
